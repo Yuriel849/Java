c642c4735c9248f892eb3069c70214af520f92bd
Yuriel849 <yuriel849sp@gmail.com>
2020-03-02 22:55:34 +0100

    toString() modified

--- Studies/src/pool/Point.java
+++ Studies/src/pool/Point.java
@@ -44,7 +44,7 @@ public class Point {
 	}
 	
 	public String toString() {
-		return "x : " + x + ", y : " + y;
+		return "( x : " + x + ", y : " + y + " )";
 	}
 	
 	public double getDistance(Point p) {
