aaccc8817be66cc0cd142bb73f988ae204ab8037
Yuriel849 <yuriel849sp@gmail.com>
2020-03-10 21:43:35 +0100

    Juice class for pool

---
+++ Studies/src/pool/Juice.java
@@ -0,0 +1,13 @@
+package pool;
+
+public class Juice {
+	String name;
+	
+	public Juice(String name) {
+		this.name = name + "Juice";
+	}
+	
+	public String toString() {
+		return name;
+	}
+} // Juice 클래스 끝.
