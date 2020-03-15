abbf6f3d6286191f4abe43b37763a93fb7b9d11a
Yuriel849 <yuriel849sp@gmail.com>
2020-03-15 18:39:28 +0100

    New BlueJ project (for HAW IE Software Constructions 2)

---
+++ BlueJ/edu/haw/main/HelloWorld.java
@@ -0,0 +1,19 @@
+package edu.haw.main;
+
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Yuriel
+ * @date 14.03.2020
+ */
+public class HelloWorld
+{
+    /**
+     * Main method to say hello to the world
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
