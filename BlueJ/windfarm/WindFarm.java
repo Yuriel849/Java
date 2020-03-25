6cf0f62819966fab15273bf77195efeae5e0deae
Yuriel849 <yuriel849sp@gmail.com>
2020-03-26 00:06:25 +0100

    2nd lab - complete

---
+++ BlueJ/windfarm/WindFarm.java
@@ -0,0 +1,87 @@
+/**
+ * Write a description of class WindFarm here.
+ *
+ * @author Yuriel
+ * @date 25.03.2020
+ */
+public class WindFarm
+{
+    // Name of the wind farm
+    private String name;
+    // Name of the country (location of wind farm)
+    private String country;
+    // Wind farm's generation capacity in MW
+    private double capacity;
+    // Number of turbines
+    private int numberTurbines;
+    // Year of commissioning
+    private int year;
+    
+    /**
+     * Constructor for objects of class WindFarm
+     */
+    public WindFarm(String name, String country, double capacity, int numberTurbines, int year)
+    {
+        this.name = name;
+        this.country = country;
+        this.capacity = capacity;
+        this.numberTurbines = numberTurbines;
+        this.year = year;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getCountry()
+    {
+        return country;
+    }
+    
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+    
+    public double getCapacity()
+    {
+        return capacity;
+    }
+    
+    public void setCapacity(double capacity)
+    {
+        this.capacity = capacity;
+    }
+    
+    public int getNumberTurbines()
+    {
+        return numberTurbines;
+    }
+    
+    public void setNumberTurbines(int numberTurbines)
+    {
+        this.numberTurbines = numberTurbines;
+    }
+    
+    public int getYear()
+    {
+        return year;
+    }
+    
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+    
+    public void print()
+    {
+        System.out.printf("%-20s %-10.0f %-15s %-10d %-6d \n",
+            name, capacity, country, numberTurbines, year);
+    }
+}
---
+++ BlueJ/windfarm/WindFarmTable.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+/**
+ * Write a description of class WindFarmTable here.
+ *
+ * @author Yuriel
+ * @date 25.03.2020
+ */
+public class WindFarmTable
+{
+    // Name of the table
+    private String name;
+    // Array with data on wind farms
+    private ArrayList<WindFarm> array;
+
+    /**
+     * Constructor for objects of class WindFarmTable
+     */
+    public WindFarmTable(String name)
+    {
+        this.name = name;
+        array = new ArrayList<WindFarm>();
+    }
+
+    /**
+     * Adds the given wind farm to an ArrayList
+     */
+    public void addPowerStation(WindFarm powerStation)
+    {
+        array.add(powerStation);
+    }
+    
+    /**
+     * Gets the total power of all the wind farms
+     */
+    public double getTotalPower()
+    {
+        double totalPower = 0;
+        
+        for(WindFarm farm : array)
+        {
+            totalPower += farm.getCapacity();
+        }
+        
+        return totalPower;
+    }
+    
+    /**
+     * Print the wind farm table information to the terminal window
+     * by delegating the printing of the rows to the WindFarm objects stored in the ArrayList<WindFarm>.
+     */
+    
+    public void printTable()
+    {
+        System.out.printf("%-20s %-10s %-15s %-10s %-6s\n",
+            "Name", "Capacity", "Country", "Turbines", "Year");
+        
+        for(WindFarm farm : array)
+        {
+            farm.print();
+        }
+    }
+}
