3fbf39cba3d477c560e109dde03f961a191a7b4c
Yuriel849 <yuriel849sp@gmail.com>
2020-03-28 00:15:11 +0100

    PowerPlant class

---
+++ BlueJ/nuclear-power-plant/PowerPlant.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class PowerPlant here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PowerPlant
+{
+    private String name;
+    private String country;
+    private double capacity;
+    private int constructionStart;
+    private int connectionYear;
+    
+    /**
+     * Constructor for objects of class PowerPlant
+     */
+    public PowerPlant(String name, String country, double capacity, int constructionStart)
+    {
+        this.name = name;
+        this.country = country;
+        this.capacity = capacity;
+        this.constructionStart = constructionStart;
+        this.connectionYear = -1;
+    }
+    
+    public PowerPlant(String name, String country, double capacity, int constructionStart, int connectionYear)
+    {
+        this(name, country, capacity, constructionStart);
+        this.connectionYear = connectionYear;
+        
+    }
+
+    /**
+     * 
+     */
+    public void print()
+    {
+        System.out.printf("%-14s %-12s %-20s %-22s %-14s\n", "Power Station", "Capacity", "Construction Start", "Connection Year", "Country");
+        System.out.printf("%-14s %-12s %-20s %-22s %-14s\n", name, capacity, constructionStart, connectionYear, country);
+    }
+}
