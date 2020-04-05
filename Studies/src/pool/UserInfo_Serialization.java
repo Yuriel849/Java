bbf32af48e6aee73159a4034a561728ef973ec4f
Yuriel849 <yuriel849sp@gmail.com>
2020-04-05 10:44:07 +0200

    moved to pool

--- Studies/src/pool/UserInfo_Serialization.java
+++ Studies/src/pool/UserInfo_Serialization.java
@@ -1,18 +1,23 @@
-package io;
+package pool;
 
-// Ex07_Serialization02 * Ex08_Serialization03에서 사용할 UserInfo를 넣는 클래스이다.
+// Class used by IO_Serialization_02 & IO_Serialization_03
 
+// java.io.Serializable interface contains nothing but indicates this class is serializable
 @SuppressWarnings("serial")
-class Ex11_Serialization01 implements java.io.Serializable {
+public class UserInfo_Serialization implements java.io.Serializable {
 	String name;
-	transient String password; // "transient" 붙였기에 "password"라는 이름을 가진 이 문자열은 직렬화에서 제외된다!
-	int age;				   // 	 >> 실제로 역직렬화해서 보면, "password"가 들어갈 자리에 기본값 "null"이라고 써있다!	
+	transient String password;
+	int age;
+	/* Password should not be exposed
+	 * 	-> Excluded from serialization because of "transient" keyword
+	 * 	-> Default value of "null" when deserialized
+	 */
 	
-	public Ex11_Serialization01() {
+	public UserInfo_Serialization() {
 		this("Unknown", "1111", 0);
 	}
 	
-	public Ex11_Serialization01(String name, String password, int age) {
+	public UserInfo_Serialization(String name, String password, int age) {
 		this.name = name;
 		this.password = password;
 		this.age = age;
@@ -21,4 +26,4 @@ class Ex11_Serialization01 implements java.io.Serializable {
 	public String toString() {
 		return "(" + name + "," + password + "," + age + ")";
 	}
-} // 클래스 끝.
+}
