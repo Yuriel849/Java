d4474c7c3b39853d654c900447e49ffc7ca97518
Yuriel849 <yuriel849sp@gmail.com>
2020-04-08 22:56:17 +0200

    clean up code

--- Studies/src/exercises/byChapter/chapter_07/Exercise07_19.java
+++ Studies/src/exercises/byChapter/chapter_07/Exercise07_19.java
@@ -11,75 +11,62 @@ class Exercise07_19 {
 		b.buy(new Computer());
 		b.buy(new Computer());
 		
-		
 		b.summary();
 	}
 }
 
 class Buyer {
 	int money = 1000;
-	Product[] cart = new Product[3]; // ������ ��ǰ�� �����ϱ� ���� �迭
-	int idx = 0; // Product �迭 card�� ���� index
+	Product[] cart = new Product[3]; // Product type array of size 3
+	int idx = 0; // Index for the array "cart"
 	
 	void buy(Product p) {
-		// ���� ���� ������ ������ ���Ͽ� ���� ���� ������ �޼��带 �����Ѵ�.
-		if(p.price > money) {
-			System.out.println("�ܾ��� �����Ͽ� " + p + "�� �� �� �����ϴ�.");
+		if(p.price > money) { // If the buyer doesn't have enough money...
+			System.out.println("You only have " + money + " euros, you cannot afford this " + p + ".");
 			return;
 		}
 		
-		// ���� ���� ����ϸ�, ��ǰ�� ������ ���� ������ ����.
 		money -= p.price;
 		
-		// ��ٱ��Ͽ� ������ ������ ��´�. (add�޼��� ȣ��)
 		add(p);
-	} // buy() ��.
+	} // buy()
 	
 	void add(Product p) {
-		// idx�� ���� ��ٱ����� ũ�⺸�� ���ų� ũ�� -->
-		if(idx >= cart.length) {
-				// ������ ��ٱ��Ϻ��� 2�� ū ���ο� �迭�� �����Ѵ�.
+		if(idx >= cart.length) { // If the cart is not large enough...
 			Product[] temp = new Product[cart.length * 2];
-				// ������ ��ٱ����� ������ ���ο� �迭�� �����Ѵ�.
 			System.arraycopy(cart, 0, temp, 0, cart.length);
-				// ���ο� ��ٱ��Ͽ� ������ ��ٱ��ϸ� �ٲ۴�.
 			cart = temp;
 		}
 
-		// ������ ��ٱ���(cart)�� �����Ѵ�. �׸��� idx�� ���� 1 ������Ų��.
 		cart[idx++] = p;
-	} // add() ��.
+	} // add()
 	
 	void summary() {
-		// ��ٱ��Ͽ� ��� ���ǵ��� ����� ����� ����Ѵ�.
-		String str = "������ ���� : ";
+		String str = "Cart Summary : ";
 		for(int i = 0; i < cart.length; i++) {
 			if(i > 0) { str += ", "; }
 			str += cart[i];
 		}
 		System.out.println(str);
 		
-		// ��ٱ��Ͽ� ��� ���ǵ��� ������ ��� ���ؼ� ����Ѵ�.
-		str = "����� �ݾ� : ";
+		str = "Total price : ";
 		int sum = 0;
 		for(int i = 0; i < cart.length; i++) {
 			sum += cart[i].price;
 		}
 		System.out.println(str + sum);
 		
-		// ������ ��� ���� �ݾ�(money)�� ����Ѵ�.
-		System.out.println("���� �ݾ� : " + money);
-		
-	} // summary() ��.
-} // Buyer Ŭ���� ��.
+		System.out.println("Remaining money : " + money);
+	} // summary()
+} // Buyer class
 
 class Product {
-	int price; // ��ǰ�� ����
+	int price;
 	
 	Product(int price) {
 		this.price = price;
 	}
-} // Product Ŭ���� ��.
+} // Product class
 
 class Tv extends Product {
 	Tv() {
@@ -89,7 +76,7 @@ class Tv extends Product {
 	public String toString() {
 		return "Tv";
 	}
-} // Tv Ŭ���� ��.
+} // Tv class
 
 class Computer extends Product {
 	Computer() {
@@ -99,7 +86,7 @@ class Computer extends Product {
 	public String toString() {
 		return "Computer";
 	}
-} // Computer Ŭ���� ��.
+} // Computer class
 
 class Audio extends Product {
 	Audio() {
@@ -109,4 +96,4 @@ class Audio extends Product {
 	public String toString() {
 		return "Audio";
 	}
-} // Audio Ŭ���� ��.
+} // Audio class
