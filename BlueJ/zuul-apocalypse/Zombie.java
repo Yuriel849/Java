99fbdb68c64e6a8a8a5acb3c437063088001ca82
Yuriel849 <yuriel849sp@gmail.com>
2020-04-29 11:30:49 +0200

    zombie health methods added

--- BlueJ/zuul-apocalypse/Zombie.java
+++ BlueJ/zuul-apocalypse/Zombie.java
@@ -8,6 +8,7 @@ public class Zombie
 {
     private String name;
     private String description;
+    private int health;
 
     /**
      * Default constructor for objects of class Zombie.
@@ -26,6 +27,7 @@ public class Zombie
     {
         this.name = name;
         this.description = description;
+        health = 100;
     }
     
     /**
@@ -44,5 +46,33 @@ public class Zombie
     public String getDescription()
     {
         return description;
+    }
+    
+    /**
+     * Get's the health of the zombie. Health is 100 at full health.
+     * @return The health of the zombie.
+     */
+    public int getHealth()
+    {
+        return health;
+    }
+
+    /**
+     * Reduce the zombie's health.
+     * @param damage The damage done to the zombie's health.
+     * @return True, if the zombie's health is reduced to 0 or less. Then the zombie should be dead. False, if the zombie's health is greater than 0.
+     */
+    public boolean reduceHealth(int damage)
+    {
+        boolean result = false;
+        
+        health -= damage;
+        
+        if(health <= 0)
+        {
+            result = true;
+        }
+        
+        return result;
     }
 }
