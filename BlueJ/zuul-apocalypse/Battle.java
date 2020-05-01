f0bdaf022f24ed5c758cf0116430a96e7080227e
Yuriel849 <yuriel849sp@gmail.com>
2020-05-01 22:30:27 +0200

    Battle class implemented

--- BlueJ/zuul-apocalypse/Battle.java
+++ BlueJ/zuul-apocalypse/Battle.java
@@ -1,12 +1,54 @@
+import java.util.ArrayList;
+
 /**
  * Class Battle
  *
  * @author Yuriel
- * @version 2020.04.29
+ * @version 2020.05.01
  */
 public class Battle
 {
-    public static boolean attack(Zombie target)
+    public static String fight(Player player, Parser parser)
+    {
+        ArrayList<Zombie> zombies = player.getCurrentRoom().getAllZombies();
+        int index = zombies.size() - 1, health = player.getHealth();
+        Zombie target = zombies.get(index);
+        String result = "", commandWord = "";
+        
+        while(index > 0 || health > 0)
+        {
+            commandWord = parser.getCommand().getCommandWord();
+            
+            if(commandWord.equals("run"))
+            {
+                System.out.println("You ran away from the zombies.");
+                result = "back";
+                break;
+            }
+            else if(commandWord.equals("attack"))
+            {
+                attack(target);
+                zombieAttack(health);
+            }
+            
+            if(health <= 0)
+            {
+                System.out.println("You are dead...");
+                result = "dead";
+                break;
+            }
+            else if(target.getHealth() <= 0)
+            {
+                System.out.println("The zombie is dead!");
+                player.getCurrentRoom().removeZombie(target);
+                target = zombies.get(--index);
+            }
+        }
+        
+        return result;
+    }
+    
+    private static boolean attack(Zombie target)
     {
         boolean zombieIsDead = false;
         int damage = (int) (Math.random() * 40);
@@ -17,14 +59,14 @@ public class Battle
         }
         else
         {
-            System.out.println("Hit! " + damage + " damage inflicted. Zombie's health is now " + target.getHealth() + ".");
             zombieIsDead = target.reduceHealth(damage);
+            System.out.println("Hit! " + damage + " damage inflicted. Zombie's health is now " + target.getHealth() + ".");
         }
         
         return zombieIsDead;
     }
     
-    public static int zombieAttack(int health)
+    private static int zombieAttack(int health)
     {
         int playerHealth = 0, damage = (int) (Math.random() * 15);
         
@@ -39,8 +81,5 @@ public class Battle
         }
         
         return playerHealth;
-    }    
-    
-    // If player's health is less than or equal to 0, game over
-    // If zombie's health is less than or equal to 0, zombie removed from fight and current room
+    }
 }
