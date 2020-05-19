4f5e863f7a5a53ca2ec00ec4352bae72a1c2e953
Yuriel849 <yuriel849sp@gmail.com>
2020-05-19 17:57:06 +0200

    PowerPlant class inherits Facility superclass

--- BlueJ/windfarm vLab05/PowerPlant.java
+++ BlueJ/windfarm vLab05/PowerPlant.java
@@ -4,12 +4,8 @@
  * Mo
  * 01/04/2020
  */
-public class PowerPlant implements IWindFarm
+public class PowerPlant extends Facility
 {
-    // instance variables - replace the example below with your own
-    private String name;
-    private String country;
-    private double capacity;
     private int constructionStart;
     private int connectionYear;
 
@@ -18,20 +14,11 @@ public class PowerPlant implements IWindFarm
      */
     public PowerPlant(String name, String country, double capacity, int constructionStart, int connectionYear) 
     {
-        // initialise instance variables
-        this.name = name;
-        this.country = country;
-        this.capacity = capacity;
+        super(name, country, capacity);
         this.constructionStart = constructionStart;
         this.connectionYear = connectionYear;
     }
     
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public void print()
     {
         //String heading = String.format("%-20s | %-15s | %-12s | %-15s | %-15s", "Name", "Country", "Capacity", "Contstruction Date", "Connection Date");
@@ -40,16 +27,5 @@ public class PowerPlant implements IWindFarm
         
         // print list
         System.out.println(row);
-    }
-    
-    /**
-     * returns private  field  capcity when called
-     *
-     * @param  
-     * @return capacity
-     */
-    public double getCapacity()
-    {
-        return this.capacity;
     }
 }
