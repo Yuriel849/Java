96ae6ebe481ae3a5df3d22609ee44dab4b7bdd69
Yuriel849 <yuriel849sp@gmail.com>
2020-05-20 21:30:42 +0200

    printHeader() formatted

--- BlueJ/windfarm vLab05/common/Facility.java
+++ BlueJ/windfarm vLab05/common/Facility.java
@@ -10,8 +10,10 @@ public interface Facility
 {
     public static void printHeader()
     {
-        System.out.printf("| %-20s %-10s %-15s %-10s %-6s |\n",
-            "Name", "Capacity", "Country", "Turbines", "Year");
+        System.out.printf("| %-30s | %-20s | %-15s | %-16s | %-18s | %-18s | %-18s | %-10s |\n",
+            "Name", "Country", "Capacity", "No. of Turbines", "Year commissioned",
+            "Construction Start", "Planned Connection", "Land Size");
+        System.out.print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
     
     // Abstract methods called in the WindFarmTable class
