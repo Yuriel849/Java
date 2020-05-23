7a0659345d749ec57039dba1d3e69e871d31e303
Mohamed ElGhamrawy <mhelghamrawy@gmail.com>
2020-05-23 19:29:44 +0200

    Solar parks added to initList

--- BlueJ/windfarm vLab05/implementation/SolarPark.java
+++ BlueJ/windfarm vLab05/implementation/SolarPark.java
@@ -15,10 +15,21 @@ public class SolarPark extends GenericFacility
     /**
      * Constructor for objects of class SolarPark
      */
-    public SolarPark(String name, String country, double capacity, int landSize, int connectionYear)
+    public SolarPark(String name, String country, double capacity, int connectionYear, int landSize)
     {
         super(name, country, capacity);
         this.landSize = landSize;
         this.connectionYear = connectionYear;
+    }
+    
+    public void print()
+    {
+        //String x = "| %-20s | %-20s | %-15s | %-15s | %-18s | %-18s | %-18s | %-9s |\n"
+    	
+    	String row = String.format("| %-20s | %-20s | %-15s | %-15s | %-18s | %-18s | %-18s | %-9s |\n",
+        		getName(), getCountry(), getCapacity(), "N/A", "N/A", "N/A", this.connectionYear, this.landSize);
+        
+        // print list
+        System.out.print(row);
     }
 }
