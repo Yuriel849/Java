3757895a49e7e52892032cc584d87b7f0672d1f2
Yuriel849 <yuriel849sp@gmail.com>
2020-06-03 15:02:25 +0200

    UI & UserInterfaceable interfaces combined

--- BlueJ/zuul-apocalypse-v2/common/UserInterfaceable.java
+++ BlueJ/zuul-apocalypse-v2/common/UserInterfaceable.java
@@ -1,14 +1,33 @@
-package common; 
+package common;
 
-// 
 /**
- * Interface UserInterfaceable here connects the frontend and the backend differently.
+ * Interface UserInterfaceable
+ * Once enabled, will display text and change images in the GUI.
  *
- * @author Mo
- * @version 2020.06.02
+ * @author Yuriel and Mo
+ * @version 2020.06.03
  */
 public interface UserInterfaceable {
-    void showCommands();
-    Command getCommand();
-    //void updateImg(String filePath); 
+//    void showCommands();
+//    Command getCommand();
+    
+    /**
+     * Displays text about the game in a JTextArea in the GUI.
+     *
+     * @param msg The text to be displayed.
+     */
+    void print(String text);
+    
+    /**
+     * Changes the image displayed in the GUI.
+     * 
+     * @param fileName The name of the image file to be displayed.
+     */
+    void changeImage(String fileName);
+    
+    /**
+     * Start the user interface for interaction with the user.
+     *
+     */
+    void enable();
 }
