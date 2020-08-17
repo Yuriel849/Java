00b33ba85281276ce0a8f3815daa69ba75b5b4ef
Yuriel849 <yuriel849sp@gmail.com>
2020-08-17 22:56:48 +0200

    Add equals and clone methods (deepcopy)

--- Data Structures & Algorithms/DS&A Studies/src/SinglyLinkedList.java
+++ Data Structures & Algorithms/DS&A Studies/src/SinglyLinkedList.java
@@ -1,4 +1,4 @@
-public class SinglyLinkedList<E> {
+public class SinglyLinkedList<E> implements Cloneable {
     /* nested Node class
         -> Strong encapsulation, internal implementation of nodes is hidden from the outside.
         -> Easy to differentiate this node type from nodes used in other classes or structures.
@@ -71,5 +71,49 @@ public class SinglyLinkedList<E> {
         if(size == 0)
             tail = null;
         return answer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == null)                  // Null check of "o".
+            return false;
+        if(getClass() != o.getClass()) // Check if the two instances are of the same class.
+            return false;
+
+        // Use nonparameterized type; with "TYPE ERASURE" it is impossible to detect at runtime if lists have same type.
+        SinglyLinkedList other = (SinglyLinkedList) o;
+        if(size != other.size)
+            return false;
+
+        Node walkA = head;       // To traverse the current list.
+        Node walkB = other.head; // To traverse the second list, "o".
+
+        while(walkA != null) {
+            if(!walkA.getElement().equals(walkB.getElement())) // If two lists have incompatible types, detected HERE.
+                return false;
+            walkA = walkA.getNext();
+            walkB = walkB.getNext();
+        }
+        return true;
+    }
+
+    public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
+        // Always use the inherited Object.clone() to create the initial shallow copy
+        SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
+
+        if(size > 0) {
+            other.head = new Node<>(head.getElement(), null);
+            Node<E> walk = head.getNext();
+            Node<E> otherTail = other.head;
+
+            while(walk != null) {
+                Node<E> newest = new Node<>(walk.getElement(), null);
+                otherTail.setNext(newest);
+                otherTail = newest;
+                walk = walk.getNext();
+            }
+        }
+
+        return other;
     }
 }
