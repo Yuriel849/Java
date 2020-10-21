ac6998151e2eb4033936590cdc75d72ddff184c9
Yuriel849 <yuriel849sp@gmail.com>
2020-10-21 21:22:02 +0200

    Project deleted as unnecessary

--- Data Structures & Algorithms/DS&A Studies/src/IntelliJ_test.java
+++
@@ -1,5 +0,0 @@
-public class IntelliJ_test {
-    public static void main(String[] args) {
-        System.out.println("Hello world; IntelliJ test class");
-    }
-}
--- Data Structures & Algorithms/DS&A Studies/src/mixin/Insurable.java
+++
@@ -1,7 +0,0 @@
-package mixin;
-
-// Interface for objects that can be insured; inherits both Sellable & Transportable interfaces.
-public interface Insurable extends Sellable, Transportable {
-    // Returns insured value in cents.
-    int insuredValue();
-}
