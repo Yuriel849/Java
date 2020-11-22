0203b11dc14410b8f0f8c1b077bcf11dcba11b60
Yuriel849 <yuriel849sp@gmail.com>
2020-11-22 23:50:54 +0100

    Test insertion sort

--- Algorithms & Data Structures/Record(Lab03)/src/InsertionTest.java
+++ Algorithms & Data Structures/Record(Lab03)/src/InsertionTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class InsertionTest {
-  
+    @Test
+    void sort() {
+        int N = 4;
+
+        int cmp = 3; // Theoretical value for compare (N - 1)
+        int cpy = 0; // Theoretical value for copy 0
+
+        Records records = new Records(N);
+        records.shuffle();
+        records.insertionSort();
+        assertEquals(cmp, Sort.getCompares());
+        assertEquals(cpy, Sort.getCopies());
+    }
 }
