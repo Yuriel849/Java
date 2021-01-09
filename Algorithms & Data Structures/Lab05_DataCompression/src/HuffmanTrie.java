4afebbb018f256b8f476038d5226d5cf000da487
Yuriel849 <yuriel849sp@gmail.com>
2021-01-09 15:47:11 +0100

    Add encode() and setCharBinaryCodes() to encode String with given Huffman tree

--- Algorithms & Data Structures/Lab05_DataCompression/src/HuffmanTrie.java
+++ Algorithms & Data Structures/Lab05_DataCompression/src/HuffmanTrie.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Huffman Trie class.
  *
@@ -10,6 +13,8 @@
 public class HuffmanTrie {
     // Alphabet size of extended ASCII
     private static final int R = 256;
+    // Lookup table matching characters and their Huffman trie binary codes
+    private static HashMap<Character, String> charBinaryCodes = new HashMap<>();
 
     // Node inner class for Huffman trie
     private static class Node implements Comparable<Node> {
@@ -64,7 +69,7 @@ public class HuffmanTrie {
 
     /**
      * Encodes a Huffman trie into a String, by going over the trie in pre-order traversal.
-     * THe trie's inner nodes are represented by a star ("*").
+     * The trie's inner nodes are represented by a star ("*").
      *
      * @param root The root of the Huffman trie to be encoded.
      * @return The String representation of the encoded Huffman trie.
@@ -74,7 +79,6 @@ public class HuffmanTrie {
     }
 
     private static String preorderTraversal(Node node) {
-        System.out.print(node.ch);
         String value = "" + node.ch;
         if(node.left != null)
             value += preorderTraversal(node.left);
@@ -83,17 +87,45 @@ public class HuffmanTrie {
         return value;
     }
 
+    public static String encode(Node root, String source) {
+        setCharBinaryCodes(root, new StringBuilder());
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            result.append(charBinaryCodes.get(c));
+        }
+
+        return result.toString();
+    }
+
+    private static void setCharBinaryCodes (Node node, StringBuilder prefix) {
+        if (node != null) {
+            if (node.left == null && node.right == null) {
+                charBinaryCodes.put(node.ch, prefix.toString());
+            } else {
+                prefix.append('0');
+                setCharBinaryCodes(node.left, prefix);
+                prefix.deleteCharAt(prefix.length() - 1);
+                prefix.append('1');
+                setCharBinaryCodes(node.right, prefix);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String s1 = "she_sells_sea_shells_by_the_seashore";
         String s2 = "selly_sells_her_shorts_by_the_seattle_store";
 
-        // Generate tree T1
-        String t1 = encodeTrie(constructTrie(s1));
-        System.out.println("\n" + t1);
-        // Generate tree T3
-//        String t3 = encodeTrie(constructTrie(s2));
-//        System.out.println(t3);
+        Node root = constructTrie(s1); // Generate tree T1
+        String t1 = encodeTrie(root);  // Generate String representation of T1
+        String encodedS1 = encode(root, s1); // Encode s1 with T1
+        System.out.printf("T1 as String : %s \ns1 encoded : %s \nNumber of bits : %d \n", t1, encodedS1, encodedS1.length());
 
-
+        root = constructTrie(s2); // Generate tree T1
+        String t3 = encodeTrie(root);  // Generate String representation of T1
+        String encodedS2 = encode(root, s2); // Encode s1 with T1
+        System.out.printf("T3 as String : %s \ns2 encoded : %s \nNumber of bits : %d \n", t3, encodedS2, encodedS2.length());
     }
 }
