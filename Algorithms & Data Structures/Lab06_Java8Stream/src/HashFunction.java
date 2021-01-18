2619e699107e9135511c30cd479637ce8d74e0cc
Yuriel849 <yuriel849sp@gmail.com>
2021-01-18 17:16:36 +0100

    New project for Lab 06, Java 8 Streams & Lambdas

---
+++ Algorithms & Data Structures/Lab06_Java8Stream/src/HashFunction.java
@@ -0,0 +1,48 @@
+public class HashFunction {
+    private String s;
+    private static final int A = 13;
+    private static final int M = 31;
+
+    /**
+     * Constructor for objects of class HashFunction
+     */
+    public HashFunction(String s) {
+        this.s = s;
+    }
+
+    public int hashCode() {
+        int hash = 0, x = 0, newHash = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isUpperCase(s.charAt(i))) { // Uppercase letters
+                x = (s.charAt(i) - 38);
+                newHash = x % M;
+                hash += newHash ;
+            } else if(Character.isLowerCase(s.charAt(i))) { // Lowercase letters
+                x = (s.charAt(i) - 96);
+                newHash = x % M;
+                hash += newHash ;
+            }
+        }
+        return A * hash;
+    }
+
+    @Override
+    public String toString() {
+        return ""+hashCode();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "he";
+        System.out.println(s1 + " : " + new HashFunction(s1).toString());
+        String s2 = "is";
+        System.out.println(s2 + " : " + new HashFunction(s2).toString());
+        String s3 = "Art";
+        System.out.println(s3 + " : " + new HashFunction(s3).toString());
+        String s4 = "has";
+        System.out.println(s4 + " : " + new HashFunction(s4).toString());
+        String s5 = "Hat";
+        System.out.println(s5 + " : " + new HashFunction(s5).toString());
+        String s6 = "this";
+        System.out.println(s6 + " : " + new HashFunction(s6).toString());
+    }
+}
