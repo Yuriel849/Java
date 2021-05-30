9d8d0517677bb4a6ea9ef8b1dc298f259b763744
Yuriel849 <yuriel849sp@gmail.com>
2021-05-30 02:12:04 +0200

    Vertical prototype for SE lab 5 preparation

---
+++ IE4-SE-Lab05/src/Coordinates.java
@@ -0,0 +1,20 @@
+public class Pair {
+    String x;
+    String y;
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+}
